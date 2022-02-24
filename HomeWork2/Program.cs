2144b48a4dfd44696c147a07c6d71338d5ccaf46
Денис Cтоляров <denis2002.stolyarov@mail.ru>
2022-02-24 22:35:36 +0700

    Добавьте файлы проекта.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(RomanNumber.Plus(new RomanNumber(15), new RomanNumber(18)));
+            try
+            {
+                Console.WriteLine(RomanNumber.Minus(new RomanNumber(18), new RomanNumber(15)));
+            }
+            catch (RomanNumberException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(RomanNumber.Multy(new RomanNumber(35), new RomanNumber(10)));
+            try
+            {
+                Console.WriteLine(RomanNumber.Divid(new RomanNumber(70), new RomanNumber(35)));
+            }
+            catch (RomanNumberException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            RomanNumber[] romanNumberArray = new RomanNumber[10];
+            for (int i = 0; i < 10; i++)
+            {
+                ushort rand = (ushort)(new Random().Next(1, 1000));
+                romanNumberArray[i] = new RomanNumber(rand);
+                Console.Write(romanNumberArray[i] + " ");
+            }
+            Console.WriteLine();
+
+            Array.Sort(romanNumberArray);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(romanNumberArray[i] + " ");
+            }
+
+
+        }
+    }
+}
---
+++ HomeWork2/RomanNumber.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    public class RomanNumber : ICloneable, IComparable
+    {
+        readonly ushort number;
+
+        public RomanNumber(ushort n)
+        {
+            if (n <= 0)
+            {
+                throw new RomanNumberException("Результат будет нецелым");
+            }
+            number = n;
+        }
+        public static RomanNumber Plus(RomanNumber n1, RomanNumber n2)
+        {
+            return new RomanNumber((ushort)(n1.number + n2.number));
+        }
+        public static RomanNumber Minus(RomanNumber n1, RomanNumber n2)
+        {
+            if (n1.number <= n2.number) throw new RomanNumberException("Результат будет отрицательный");
+            return new RomanNumber((ushort)(n1.number - n2.number));
+        }
+        public static RomanNumber Multy(RomanNumber n1, RomanNumber n2)
+        {
+            return new RomanNumber((ushort)(n1.number * n2.number));
+        }
+        public static RomanNumber Divid(RomanNumber n1, RomanNumber n2)
+        {
+            return new RomanNumber((ushort)(n1.number / n2.number));
+        }
+        public override string ToString()
+        {
+            return toRoman(number);
+        }
+        private static string toRoman(int number)
+        {
+
+            string tmp = "";
+            while (number >= 1000)
+            {
+                tmp += "M";
+                number -= 1000;
+            }
+            while (number >= 900)
+            {
+                tmp += "CM";
+                number -= 900;
+            }
+            while (number >= 500)
+            {
+                tmp += "D";
+                number -= 500;
+            }
+            while (number >= 400)
+            {
+                tmp += "CD";
+                number -= 400;
+            }
+            while (number >= 100)
+            {
+                tmp += "C";
+                number -= 100;
+            }
+            while (number >= 90)
+            {
+                tmp += "XC";
+                number -= 90;
+            }
+            while (number >= 50)
+            {
+                tmp += "L";
+                number -= 50;
+            }
+            while (number >= 40)
+            {
+                tmp += "XL";
+                number -= 40;
+            }
+            while (number >= 10)
+            {
+                tmp += "X";
+                number -= 10;
+            }
+            while (number >= 9)
+            {
+                tmp += "IX";
+                number -= 9;
+            }
+            while (number >= 5)
+            {
+                tmp += "V";
+                number -= 5;
+            }
+            while (number >= 4)
+            {
+                tmp += "IV";
+                number -= 4;
+            }
+            while (number >= 1)
+            {
+                tmp += "I";
+                number -= 1;
+            }
+            return tmp;
+            throw new RomanNumberException();
+        }
+
+        public object Clone()
+        {
+            return new RomanNumber(number);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is RomanNumber num) return number.CompareTo(num.number);
+            else throw new RomanNumberException("Некорректное значение аргумента");
+        }
+
+    }
+}
